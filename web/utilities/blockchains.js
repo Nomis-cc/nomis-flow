9f69de0022e5291b17c6691e5e3960aa47a06c71
Chebotov Nickolay <spiritkola@hotmail.com>
2023-02-25 16:41:48 +0300

    update: Add frontend

---
+++ web/utilities/blockchains.js
@@ -0,0 +1,13 @@
+export const blockchains = [
+  {
+    group: "finance",
+    groupLabel: "Finance",
+    item: "Flow",
+    slug: "flow",
+    apiSlug: "flow",
+    icon: "/blockchains/flow.svg",
+    placeholder: "0x... address",
+    coin: "FLOW",
+    hide: false,
+  },
+];
